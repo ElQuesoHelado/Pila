e9fb2a27cefefb1232a8f181c49d87231738f6f0
MisaelPalomino <misaeljpalominor@gmail.com>
2024-10-03 11:36:51 -0500

    Se definio Struct Pila con sus atributos y funciones

--- pila.h
+++ pila.h
@@ -1,8 +1,17 @@
 #ifndef PILA_H
 #define PILA_H
 
+#define SIZE 100
+
 typedef struct {
-  int x;
-}Pila;
+  int data[100];
+  int top;
+} Pila;
+
+void initPila(Pila *p);
+void push(Pila *p, int v);
+void pop(Pila *p);
+int top(Pila *p);
+int size(Pila *p);
 
 #endif
